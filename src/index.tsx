9e81aa270a29c2db90aedea10642dee4aa72b0a1
gomess <Sanny.Gomes@dominos.com>
2020-10-09 11:50:11 -0500

    Add more comments and rearange components.

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import "./index.css"
 type SquareValue = 'X'| 'O' | null;
 
 // Function that contains all of the possible winning combinations
+// that determines a winner.
 const calculateWinner = (squares: SquareValue[]): SquareValue => {
   const lines = [
     [0, 1, 2],
@@ -26,65 +27,25 @@ const calculateWinner = (squares: SquareValue[]): SquareValue => {
   return null;
 }
 
-// Interface : Tells us what props(or parameters) a functional component will be using.
+// Our 'SquareProps' interface
+// What is an Interface? 
+// An interface tells us what props(or parameters) a functional component will be using.
 interface SquareProps{
     onClick(): void;
     value: SquareValue;
 }
 
-// Our 'Square' component
-// This component renders a button on the screen
-// React.FC : Interface provided by Typescript to help write React components.
-const Square: React.FC<SquareProps> = props => {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-  }
-
+  // Our 'BoardProps' interface
   interface BoardProps{
     onClick(i : number): void;
     squares: SquareValue[];
   }
-  
-  // Our 'Board' component
-  // This component renders 9 'Square' component
-  const Board: React.FC<BoardProps> = props => {
-    // 'renderSquare' is a function inside of this functional component.
-    //  It returns the 'Square' component declared above. How do we do this? 
-    // By importing 'ReactNode'.
-    const renderSquare = (i: number): ReactNode => {
-      return (
-        <Square
-          value={props.squares[i]}
-          onClick={() => props.onClick(i)}
-        />
-      );
-    }
 
-    return (
-    <div>
-        <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-        </div>
-        <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-        </div>
-        <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-        </div>
-    </div>
-    );
-  }
-  
   // Our 'Game' component
+  // This component contains 3 states
+  // 1- history : Keeps track of all of our moves so far.
+  // 2- stepNumber : Our current step number
+  // 3- xIsNext : Determines if 'X' is going or 'O' is going.
   // This component renders a board with some placeholder values.
   // 'Game' does NOT take in any props.
   //  We are using the 'useState' hook to set the default values
@@ -103,8 +64,8 @@ const Square: React.FC<SquareProps> = props => {
      // This is where all of the heavy lifting happens
      // This method
      // 1- Determines if there is a winner
-     // 2- Determines who goes next
-     // 3- Keeps a history of our moves
+     // 2- Keeps a history of our moves
+     // 3- Determines who goes next
      const handleClick = (i : number): void => {
       const newHistory = history.slice(0, stepNumber + 1);
       const current = newHistory[history.length - 1];
@@ -151,6 +112,8 @@ const Square: React.FC<SquareProps> = props => {
       status = "Next player: " + (xIsNext ? "X" : "O");
     }
 
+    // The 'Game' component passes a part of the history called 'sqaures'
+    // to the 'Board'component. We also passes a 'onClick' event to the 'Board' component.
     return (
       <div className="game">
         <div className="game-board">
@@ -166,7 +129,55 @@ const Square: React.FC<SquareProps> = props => {
       </div>
     );
   };
-  
+
+   // Our 'Board' component
+  // This component renders 9 'Square' component and passes the value which could be
+  // NULL, X or O and the onClick event to the 'Square' component.
+  const Board: React.FC<BoardProps> = props => {
+    // 'renderSquare' is a function inside of this functional component.
+    // It returns the 'Square' component declared above. How do we do this? 
+    // By importing 'ReactNode'.
+    const renderSquare = (i: number): ReactNode => {
+      return (
+        <Square
+          value={props.squares[i]}
+          onClick={() => props.onClick(i)}
+        />
+      );
+    }
+
+    return (
+    <div>
+        <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+        </div>
+        <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+        </div>
+        <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+        </div>
+    </div>
+    );
+  }
+
+// Our 'Square' component
+// This component renders a button on the screen
+// React.FC : Interface provided by Typescript to help write React components.
+const Square: React.FC<SquareProps> = props => {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+
   // ========================================
   
   ReactDOM.render(<Game />, document.getElementById("root"));
